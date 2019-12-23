0daa9ade620c69c8c087654c7e0dc63e204f152b
Raghu Varma <raghuvarma331@gmail.com>
2019-12-23 21:27:18 +0530

    add telegram template

---
+++ channel.sh
@@ -0,0 +1,70 @@
+    Telegram_Api_code=
+
+
+    wget https://github.com/RaghuVarma331/scripts/raw/master/telegram.py
+    wget https://github.com/RaghuVarma331/custom_roms_banners/raw/master/image.png
+    python telegram.py -t $Telegram_Api_code -c @Nokia6plusofficial -P image.png -C " 
+    *
+    New Nokia SDM660 Flash Tool 
+    Release is Up
+    
+    $(date)*
+    
+    🔨 [Download flash tool](https://github.com/RaghuVarma331/Nokia-SDM660-Tool/releases)
+    💬 [Flashing procedure](https://raw.githubusercontent.com/RaghuVarma331/changelogs/master/crossdevelopment/hbinstallation.txt)
+    📱 Device: *Nokia 6.1 Plus*
+    ⚡Build Version: *V-1*
+    ⚡Supported Android Version: *8.0, 9.0 & 10.0*
+    👤 By: *Raghu Varma*
+    #HB #NOKIA #TOOL #GUI                                                                                                                                                                          
+    Follow: @Nokia6plusofficial ✅" 
+    
+    python telegram.py -t $Telegram_Api_code -c @Nokia7plusOfficial -P image.png -C " 
+    *
+    New Nokia SDM660 Flash Tool 
+    Release is Up
+    
+    $(date)*
+    
+    🔨 [Download flash tool](https://github.com/RaghuVarma331/Nokia-SDM660-Tool/releases)
+    💬 [Flashing procedure](https://raw.githubusercontent.com/RaghuVarma331/changelogs/master/crossdevelopment/hbinstallation.txt)
+    📱 Device: *Nokia 7 Plus*
+    ⚡Build Version: *V-1*
+    ⚡Supported Android Version: *8.0, 9.0 & 10.0*
+    👤 By: *Raghu Varma*
+    #HB #NOKIA #TOOL #GUI                                                                                                                                                                          
+    Follow: @Nokia6plusofficial ✅"     
+    
+    
+    python telegram.py -t $Telegram_Api_code -c @nokia7161 -P image.png -C " 
+    *
+    New Nokia SDM660 Flash Tool 
+    Release is Up
+    
+    $(date)*
+    
+    🔨 [Download flash tool](https://github.com/RaghuVarma331/Nokia-SDM660-Tool/releases)
+    💬 [Flashing procedure](https://raw.githubusercontent.com/RaghuVarma331/changelogs/master/crossdevelopment/hbinstallation.txt)
+    📱 Device: *Nokia 7.1*
+    ⚡Build Version: *V-1*
+    ⚡Supported Android Version: *8.0, 9.0 & 10.0*
+    👤 By: *Raghu Varma*
+    #HB #NOKIA #TOOL #GUI                                                                                                                                                                          
+    Follow: @nokia7161 ✅"      
+    
+    python telegram.py -t $Telegram_Api_code -c @nokia7161 -P image.png -C " 
+    *
+    New Nokia SDM660 Flash Tool 
+    Release is Up
+    
+    $(date)*
+    
+    🔨 [Download flash tool](https://github.com/RaghuVarma331/Nokia-SDM660-Tool/releases)
+    💬 [Flashing procedure](https://raw.githubusercontent.com/RaghuVarma331/changelogs/master/crossdevelopment/hbinstallation.txt)
+    📱 Device: *Nokia 6.1*
+    ⚡Build Version: *V-1*
+    ⚡Supported Android Version: *8.0, 9.0 & 10.0*
+    👤 By: *Raghu Varma*
+    #HB #NOKIA #TOOL #GUI                                                                                                                                                                          
+    Follow: @nokia7161 ✅"     
+    
